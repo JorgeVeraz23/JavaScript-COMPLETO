91a894b1be0c54feace2c15af3fd1157bee518d7
JorgeVeraz23 <JorgeVeraZambrano@outlook.com>
2023-06-28 22:50:26 -0500

    first commit

---
+++ js/constantes.js
@@ -0,0 +1,7 @@
+export const PI = Math.PI;
+export let usuario = "Jon";
+export let password = "qwerty";
+
+export default function saludar(){
+    console.log("Hola Modulos +ES6")
+}
---
+++ js/modulos.js
@@ -0,0 +1,11 @@
+import saludar, {PI, usuario, password} from "./constantes.js"
+import { aritmetica} from "./aritmetica.js";
+
+
+console.log("Archivo modulo.js");
+
+console.log(PI);
+console.log(usuario);
+console.log(password);
+console.log(aritmetica.sumar(5,4));
+saludar();
